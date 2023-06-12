398285ac465fe837236d3239fcda9cfc901c7a2f
Kenko Yoshitomi <universeengineer@gmail.com>
2023-06-12 09:53:39 +0900

    Add list2

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    list1();
+    // list1();
+    list2();
 }
 
 // 変数のスコープが終わると値は開放される
 fn list1() {
     {
-        let a = String::from("aaa");
+        let a = String::from("hello");
     }
-    // print!("{}", a)
+    // println!("{}", a)
+}
+
+// ムーブしたあとの変数は使えない
+fn list2() {
+    let a = String::from("hello");
+    let b = a;
+    println!("{}", b);
+    // println!("{}", a)
 }
